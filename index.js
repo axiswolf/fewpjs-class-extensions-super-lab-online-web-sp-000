695f815b00e9e25625d2eb5c912b9ebe7427a83d
Karen V <karenvo@usc.edu>
2020-01-14 07:24:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Deciduous extends Tree {
   }
   static definition() {
     return (
-      super.definition() + 'Deciduous trees shed their leaves annually.'
+      super.definition() + ' Deciduous trees shed their leaves annually.'
     )
   }
 }
@@ -31,7 +31,7 @@ class Evergreen extends Tree {
   }
   static definition() {
     return (
-      super.definition() + `Evergreens keep their leaves all year round.`
+      super.definition() + ` Evergreens keep their leaves all year round.`
     )
   }
 }
